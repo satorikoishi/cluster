37fb1d2e7b24381179f9e44def00d37219962296
Wei Jin <410453494@qq.com>
2023-03-07 19:53:01 +0800

    Seems no need for cloudburst and anna build...

--- init.sh
+++ init.sh
@@ -2,23 +2,23 @@ sudo apt update
 sudo apt install -y protobuf-compiler python3-pip
 cd ~
 
-# cloudburst
-git clone https://github.com/satorikoishi/cloudburst.git
-cd cloudburst
-pip3 install -r requirements.txt
-git submodule update --init --recursive
-./common/scripts/install-dependencies.sh
-sudo ./scripts/install-anna.sh
-./scripts/build.sh
-cd ..
+# # cloudburst
+# git clone https://github.com/satorikoishi/cloudburst.git
+# cd cloudburst
+# pip3 install -r requirements.txt
+# git submodule update --init --recursive
+# ./common/scripts/install-dependencies.sh
+# sudo ./scripts/install-anna.sh
+# ./scripts/build.sh
+# cd ..
 
-# anna
-git clone --recurse-submodules https://github.com/satorikoishi/anna.git
-cd anna
-./scripts/build.sh -g -j8
-cd ..
+# # anna
+# git clone --recurse-submodules https://github.com/satorikoishi/anna.git
+# cd anna
+# ./scripts/build.sh -g -j8
+# cd ..
 
-# cluster
-git clone --recurse-submodules https://github.com/satorikoishi/cluster.git
+# # cluster
+# git clone --recurse-submodules https://github.com/satorikoishi/cluster.git
 
 echo 'export HYDRO_HOME=~' >> ~/.bashrc
