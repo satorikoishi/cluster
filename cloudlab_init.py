c47afc484ba439e9c0cb4e6e98ccc6db34f31a3f
Wei Jin <410453494@qq.com>
2023-03-09 09:35:16 +0800

    cluster init

--- cloudlab_init.py
+++ cloudlab_init.py
@@ -2,6 +2,7 @@ from fabric import Connection
 from fabric.group import ThreadingGroup
 
 init_cmd = 'git clone https://github.com/satorikoishi/cluster.git && ~/cluster/init.sh && ~/cluster/k8s.sh'
+label_dict = {1:'general', 2:'routing', 3:'memory', 4:'function', 5:'scheduler', 6:'benchmark'}
 
 class Node(object):
     def __init__(self, id, username, port, host):
@@ -38,6 +39,32 @@ def batch_init():
     print("Batch init start")
     g_host.run(init_cmd)
     print("Batch init finished")
+
+def cluster_init():
+    all_conns = get_host_connections()
+    master_conn = all_conns[0]
+    worker_conn = all_conns[1:]
+    
+    master_conn.run('sudo kubeadm init')
+    master_conn.run('mkdir -p $HOME/.kube')
+    master_conn.run('sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config')
+    master_conn.run('sudo chown $(id -u):$(id -g) $HOME/.kube/config')
+    
+    join_cmd = master_conn.run('kubeadm token create --print-join-command')
+    
+    g_worker = ThreadingGroup.from_connections(worker_conn)
+    g_worker.run('sudo ' + join_cmd.stdout)
+    
+    label_nodes(master_conn, worker_conn)
+
+def label_nodes(master_conn, worker_conn):
+    nodes = master_conn.run('kubectl get nodes').stdout.split('\n')
+    node_id = 1
+    for line in nodes:
+        if 'node' + str(node_id) in line:
+            node = line.split(' ')[0]
+            master_conn.run(f'kubectl label nodes {node} role={label_dict[node_id]}')
+            node_id = node_id + 1
     
 if __name__ == "__main__":
     pass
--- fabfile.py
+++ fabfile.py
@@ -8,3 +8,7 @@ def hello(ctx):
 @task
 def init(ctx):
     cloudlab_init.batch_init()
+    
+@task
+def cluster(ctx):
+    cloudlab_init.cluster_init()
