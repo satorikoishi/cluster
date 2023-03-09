220e93f2e343d852286d2a52e7f94739ace937d5
Wei Jin <410453494@qq.com>
2023-03-09 20:00:48 +0800

    fix management ip

--- hydro/cluster/add_nodes.py
+++ hydro/cluster/add_nodes.py
@@ -54,7 +54,7 @@ def add_nodes(client, apps_client, cfile, kinds, counts, create=False,
 
     # util.run_process(['./validate_cluster.sh'])
 
-    management_ip = '127.0.0.1'
+    management_ip = util.get_pod_ips(client, 'role=management')[0]
     route_ips = util.get_pod_ips(client, 'role=routing')
 
     if len(route_ips) > 0:
