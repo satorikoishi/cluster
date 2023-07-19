cc19ac05de9a1876f63194bf56c05de7270854bf
Wei Jin <410453494@qq.com>
2023-07-19 18:48:15 +0800

    draw storage load

--- scripts/graph/draw_graph.py
+++ scripts/graph/draw_graph.py
@@ -1,4 +1,5 @@
 from matplotlib import pyplot as plt
+import pandas as pd
 
 from extract import extract_list_traversal_csv, extract_micro_csv
 
@@ -58,7 +59,32 @@ def draw_list_traversal(csv_name):
     plt.savefig(f"img/list_traversal.png")
     plt.show()
 
-
+def draw_compute_emulate_storage_load():
+    # Read Data
+    df = pd.read_csv("../data-archive/NDPFaas/compute_emulate_storage_load/2023-07-19compare/exec_detailed_latency.csv", header=None)
+    df.mask((df > 7.5), inplace=True)
+    anna_line = df.iloc[0].tolist()
+    df = df.iloc[1:, :1000]
+    shredder_line = []
+    for i in range(0, 3):
+        shredder_line = shredder_line + (df.iloc[i].tolist())
+    df = pd.read_csv("../data-archive/NDPFaas/compute_emulate_storage_load/2023-0719-disablereplica/exec_detailed_latency.csv", header=None)
+    df.mask((df > 7.5), inplace=True)
+    arbiter_line = []
+    for i in range(0, 3):
+        arbiter_line = arbiter_line + (df.iloc[i].tolist())
+    xaxis = [x for x in range(3000)]
+    
+    # Plt
+    plt.xlabel("Requests")
+    plt.ylabel("Executor Latency (ms)")
+    plt.scatter(xaxis, arbiter_line, s=0.1, label="FaaSPE")
+    plt.scatter(xaxis, anna_line, s=0.1, label="Cloudburst")
+    plt.scatter(xaxis, shredder_line, s=0.1, label="Cloudburst-S")
+    plt.legend(loc='upper right', markerscale=8)
+    plt.show()
+    
 if __name__ == "__main__":
-    draw_micro("data/micro.csv")
-    draw_list_traversal("data/list_traversal.csv")
+    # draw_micro("data/micro.csv")
+    # draw_list_traversal("data/list_traversal.csv")
+    draw_compute_emulate_storage_load()
