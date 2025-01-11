3a651581de06ad8339214a34ddbeb65953d8e386
Wei Jin <410453494@qq.com>
2025-01-11 22:43:39 +0800

    real app test

---
+++ scripts/bench/run_real_app.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import subprocess
+import sys
+import utils
+
+## Example: ./scripts/bench/run_real_app.py ap
+
+num_requests = 1000
+app_list = ['auth', 'calc-avg', 'k-hop', 'file-replicator', 'list-traversal', 'user-follow']
+
+def run_batch(bench_name, clients):
+    func_cmd = f'./scripts/run_benchmark.sh {bench_name}'
+    print(f'Test clients: {clients}')
+    
+    for app_name in app_list:
+        subprocess.run(f'{func_cmd}:1:create', shell=True)
+        for client_name in clients:
+            cmd = f'{func_cmd}:{num_requests}:{client_name}:{app_name}'
+            print(f'{cmd}')
+            subprocess.run(cmd, shell=True)
+    
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Too few args. Usage: ./scripts/bench/run_real_app.py {clients}')
+        exit(1)
+        
+    func_name = 'real_app'
+    clients = utils.parse_clients()
+    
+    run_batch(func_name, clients)
